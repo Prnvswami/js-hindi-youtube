52b5ddde65a0aa30890478fd24edd8eb5dd43d2c
Prnvswami <132643519+Prnvswami@users.noreply.github.com>
2023-10-10 04:14:09 +0000

    datatypes

---
+++ 01_basics/basics.js
@@ -0,0 +1,9 @@
+"use strict";
+let accountID="1234"
+let accountName="hue hue "
+
+console.log(accountID)
+const myObj = {firstname:"John", lastname:"Doe"};
+console.log(myObj);
+
+console.log(typeof accountName)
