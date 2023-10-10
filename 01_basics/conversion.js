18f6e18aa081b6bf633566f555527bd6baa68305
Prnvswami <132643519+Prnvswami@users.noreply.github.com>
2023-10-10 12:53:31 +0000

    converion to datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,29 @@
+/*let score=null
+
+ console.log(score)
+ console.log(typeof score) 
+
+ let v=Number(score)
+ 
+ console.log(typeof v) 
+ console.log(v) 
+
+ let acc=""
+
+let x=Boolean(acc)
+
+//true-1 false -0
+// string empty - false 
+// string -- true 
+console.log(typeof x)
+console.log(x) */
+
+let ac=33
+
+let x1=String(ac)
+
+//true-1 false -0
+// string empty - false 
+// string -- true 
+console.log(typeof x1)
+console.log(x1)
